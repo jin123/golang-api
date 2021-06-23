77efcdd360bcef7a2a7e4abaece9b0ff015b65df
jinruimin_v <jinruimin_v@didiglobal.com>
2021-06-23 18:24:41 +0800

    dix

--- controllers/test.go
+++ controllers/test.go
@@ -1,12 +1,19 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 type TestCtl struct {
 	ctl
 }
 
 func (t *TestCtl) Test1(c *gin.Context) {
+	fmt.Println(" this is ctl")
+	userId, ok := c.Get("keyUserId")
+	fmt.Println(userId, ok)
 	c.JSON(200, gin.H{
 		"message": "this is ctl test",
 	})
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,48 @@
 package routers
 
 import (
+	"fmt"
 	"golang-api/controllers"
 
 	"github.com/gin-gonic/gin"
 )
+
+//--------------------------------------------//
+type MyContext struct {
+	*gin.Context
+	userId int64
+}
+
+//
+func do(c *MyContext) {
+	fmt.Println(c.userId) //123456
+}
+
+//
+type HandlerFunc func(*MyContext)
+
+//
+func MyHandler(handler HandlerFunc) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//
+		ctx := new(MyContext)
+		ctx.Context = c
+
+		//
+		handler(ctx)
+	}
+}
+
+//--------------------------------------------//
+//
+//--------------------------------------------//
+func MyMiddleware(ctx *gin.Context) {
+	ctx.Set("mykey", 10)
+	ctx.Set("mykey2", "m1")
+	ctx.Set("keyUserId", int64(123456)) //
+}
+
+//--------------------------------------------//
 
 func StartRouters() *gin.Engine {
 	r := gin.Default()
@@ -14,12 +52,8 @@ func StartRouters() *gin.Engine {
 			"message": "pong",
 		})
 	})
-	r.GET("/test", test.Test1())
+	r.Use(gin.Recovery())
+	r.GET("/test", test.Test1)
+	r.GET("/hi", MyMiddleware, MyHandler(do))
 	return r
-}
-
-func test(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "this is test1111",
-	})
 }
